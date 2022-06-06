502acacb3223054ab02e8d8039238f32e58713de
Sercan S <83725191+ragiii@users.noreply.github.com>
2022-06-06 21:41:25 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        int n, total=0;
+        
+        Scanner input = new Scanner(System.in);
+        
+        do {
+            System.out.print("Sayi giriniz :");
+            n = input.nextInt();
+            if(n%2 == 0 && n%4 == 0)
+            {
+               total += n;
+            }
+        } while (n%2 == 0);
+        System.out.println("Sayilarin Toplami :"+total);
+    }
+}
